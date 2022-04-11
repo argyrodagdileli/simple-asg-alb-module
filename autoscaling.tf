b1e4568bd77c991ff02b1098bacc0575bbab8642
Argyro Dagdileli <argyro.dagdileli@gmail.com>
2022-04-11 05:01:00 +0300

    feat: Introduce simpe autoscaling policy

--- autoscaling.tf
+++ autoscaling.tf
@@ -52,3 +52,48 @@ resource "aws_autoscaling_attachment" "web_asg_main_alb" {
   autoscaling_group_name = aws_autoscaling_group.web.id
   lb_target_group_arn    = aws_lb_target_group.web.arn
 }
+resource "aws_autoscaling_policy" "web_policy_up" {
+  name                   = "web_policy_up"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.web.name
+}
+resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
+  alarm_name          = "web_cpu_alarm_up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "50"
+  dimensions = {
+    AutoScalingGroupName = "${aws_autoscaling_group.web.name}"
+  }
+  alarm_description = "Monitors the EC2 instance CPU utilization"
+  alarm_actions     = ["${aws_autoscaling_policy.web_policy_up.arn}"]
+}
+resource "aws_autoscaling_policy" "web_policy_down" {
+  name                   = "web_policy_down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.web.name
+}
+resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_down" {
+  alarm_name          = "web_cpu_alarm_down"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "40"
+  dimensions = {
+    AutoScalingGroupName = "${aws_autoscaling_group.web.name}"
+  }
+
+  alarm_description = "Monitors the EC2 instance CPU utilization"
+  alarm_actions     = ["${aws_autoscaling_policy.web_policy_down.arn}"]
+}
